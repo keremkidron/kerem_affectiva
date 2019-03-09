ec4c81712cda73954001df614980a437e49d388a
keremkidron <keremkidron@gmail.com>
2019-03-09 22:34:55 +0200

    kerem update

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ subject_id = 1024
 x = json.load((open('data/%d.0.json' % subject_id)))
 # x[step_id] = (time, affectiva)
 
-#  example:
+# example:
 step_id = '0'
 time_step = 0
 time_stamp = x[step_id][time_step][0]
 aff = x[step_id][time_step][1]
 expression = aff['expressions']
 emotions = aff['emotions']
-print(time_stamp)
-print(emotions[7])
+
 
 x_axis = [x[step_id][i][0] for i in range(len(x))]
 y_axis = [x[step_id][i][1]['emotions'][7] for i in range(len(x))]
 
-print(x_axis)
-print(y_axis)
+
 plt.plot(x_axis,y_axis)
+plt.xlabel('time stamp')
+plt.ylabel('surprise')
 plt.show() 

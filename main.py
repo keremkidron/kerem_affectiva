52d96a8c81133c30ca8ba7a90f34a5f4e850bf58
keremkidron <keremkidron@gmail.com>
2019-03-13 16:45:04 +0200

    kerem update

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import json
 import matplotlib.pyplot as plt
 
+
 subject_id = 1024
 
 x = json.load((open('data/%d.0.json' % subject_id)))
+
 # x[step_id] = (time, affectiva)
 
 # example:
@@ -14,12 +16,13 @@ aff = x[step_id][time_step][1]
 expression = aff['expressions']
 emotions = aff['emotions']
 
-
-x_axis = [x[step_id][i][0] for i in range(len(x))]
-y_axis = [x[step_id][i][1]['emotions'][7] for i in range(len(x))]
-
-
-plt.plot(x_axis,y_axis)
-plt.xlabel('time stamp')
-plt.ylabel('surprise')
-plt.show() 
+for step_id in x.keys():
+    if x[step_id] == {}:
+        continue
+    x_axis = [x[step_id][i][0] for i in range(len(x))]  #all time steps in step_id
+    y_axis = [x[step_id][i][1]['emotions'][7] for i in range(len(x))]   #all surprise rank in each time step
+    plt.plot(x_axis,y_axis)
+    plt.title('step %s surprise level' % step_id)
+    plt.xlabel('time stamp')
+    plt.ylabel('surprise')
+    plt.show()
